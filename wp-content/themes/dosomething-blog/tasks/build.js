32de085f80dec28e3ac3eda3045666fdbf166d1c
Tom Rosario <hello@tomrosario.com>
2014-10-21 12:36:22 -0400

    imagemin grunt task

--- wp-content/themes/dosomething-blog/tasks/build.js
+++ wp-content/themes/dosomething-blog/tasks/build.js
@@ -1,8 +1,8 @@
 module.exports = function(grunt) {
   // The `build` task lints, tests, and compiles assets.
-  grunt.registerTask("build", ["uglify:dev", "sass:prod"]);
+  grunt.registerTask("build", ["uglify:dev", "sass:prod", "imagemin:dynamic"]);
 
   // The `prod` build task is used when building for production. Since compiled assets
   // are ignored in version control, this is run in Continuous Integration on deploy.
-  grunt.registerTask("prod", ["uglify:prod", "sass:dev", "sass:prod"]);
+  grunt.registerTask("prod", ["uglify:prod", "sass:dev", "sass:prod", "imagemin:dynamic"]);
 }
